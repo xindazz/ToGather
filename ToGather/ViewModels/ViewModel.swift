0de782d27f0b761eb1179670e9720fbc782a4a5d
Lisa Leung <clleung@andrew.cmu.edu>
2022-10-30 14:49:39 -0400

    organized main branch files and boilerplate code

---
+++ ToGather/ViewModels/ViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ViewModel.swift
+//  ToGather
+//
+//  Created by Lisa Leung on 10/30/22.
+//
+
+import Foundation
+
+
+
+class ViewModel: ObservableObject {
+// MARK: this includes....
+  
+}
+// MARK: uncomment the below + add to the ViewModel class for search :), based off of SwiftRepos Lab
+// fields
+//@Published var repos: [Repository] = []
+//@Published var searchText: String = ""
+//@Published var filteredRepos: [Repository] = []
+
+//  filters the repos
+//func search(searchText: String) {
+//  self.filteredRepos = self.repos.filter { repo in
+//        return repo.name.lowercased().contains(searchText.lowercased())
+//      }
+//}
