758e58969c22a4d83d37b02f369708cdc75ad730
Lisa Leung <clleung@andrew.cmu.edu>
2022-11-03 17:03:41 -0400

    finished basic layout for dashboard, hard-coded data

--- ToGather/Views/Dashboard.swift
+++ ToGather/Views/Dashboard.swift
@@ -13,25 +13,10 @@ struct DashboardView: View {
   var body: some View {
 //    trip header and nav
 //    MARK: break down everything below into sub-files later
-//    MARK: members block
-//    MARK: trip details block
-//    category name
-//    VStack (alignment: .leading){
-//      Text("Tuesday, November 1 at 12:00pm to Friday, November 11 at 11:00am" ).font(.headline)
-//      Text("Fly to LAX from LGA").font(.subheadline)
-//      Text("Category: Travel").font(.subheadline)
-//    }
-//    VStack (alignment: .leading){
-//      Text("Tuesday, November 1 at 12:00pm to Friday, November 11 at 11:00am" ).font(.headline)
-//      Text("WHAT: Fly to LAX from LGA").font(.subheadline)
-//      Text("Category: Travel").font(.subheadline)
-//      Text("WHERE: 1 World Way, Los Angeles, CA 90045")
-//      Text("WHO: All group members")
-//      Text("HOW-MUCH: $300 - $500 per round-trip ticket")
-//      Text("WHY: Cheapest flight on Skyscanner with Spirit Airlines")
-//      Text("[SkyScanner](https://www.skyscanner.com/)")
-    
+
+// MARK: trip header
     VStack (alignment: .leading) {
+      Spacer()
       Text("NYC Trip")
         .font(.largeTitle)
         .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
@@ -39,52 +24,77 @@ struct DashboardView: View {
         .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
       
       Divider()
-      
+//    MARK: members block
       VStack (alignment: .leading){
         Text("Members").font(.headline)
           .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
         HStack {
-          Image("ben-den-engelsen-unsplash")
-            .resizable()
-            .scaledToFit()
-            .clipShape(Circle())
-            .frame(width: 150, height: 150)
+          VStack {
+            Image("ben-den-engelsen-unsplash")
+              .resizable()
+              .scaledToFit()
+              .clipShape(Circle())
+              .frame(width: 100, height: 100)
+            VStack(alignment: .trailing) {
+               Text("Brandon Woo")
+                 .font(.caption)
+             } //end VStack
+          } //end Vstack
+          VStack {
+            Image("michael-dam-unsplash")
+              .resizable()
+              .scaledToFit()
+              .clipShape(Circle())
+              .frame(width: 100, height: 100)
+            VStack(alignment: .trailing) {
+               Text("Diana Prince")
+                 .font(.caption)
+             } //end VStack
+          } //end Vstack
+          
         }//end hstack
-        .padding()
-        Spacer()
-        Text("Trip Details")
-        Text("Travel")
+        .padding(EdgeInsets(top: -10, leading: 20, bottom: 10, trailing: 0))
       } // end vstack
       
-     
-      Button(action: {}) {
-         Spacer()
-        Label("Add Trip Details", systemImage: "plus.circle")
-             .font(.headline)
-             .padding()
-             .foregroundColor(.white)
-         Spacer()
-      }
-       .background(Color(UIColor.systemBlue))
-       .clipShape(RoundedRectangle(cornerRadius: 12))
-       .padding()
-
-      Spacer()
-      Spacer()
+//    MARK: trip details block
       VStack (alignment: .leading){
-        Text("Tuesday, November 1 at 12:00pm to Friday, November 11 at 11:00am" ).font(.headline)
-        Text("WHAT: Fly to LAX from LGA").font(.subheadline)
-        Text("Category: Travel").font(.subheadline)
-        Text("WHERE: 1 World Way, Los Angeles, CA 90045")
-        Text("WHO: All group members")
-        Text("HOW-MUCH: $300 - $500 per round-trip ticket")
-        Text("WHY: Cheapest flight on Skyscanner with Spirit Airlines")
-        Text("[SkyScanner](https://www.skyscanner.com/)")
-        Image("ben-den-engelsen-unsplash")
-          .resizable()
-          .scaledToFit()
+        Text("Trip Details").font(.title)
+          .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+        
+        Button(action: {}) {
+           Spacer()
+          Label("Add Trip Details", systemImage: "plus.circle")
+               .font(.headline)
+               .padding()
+               .foregroundColor(.white)
+           Spacer()
+        }
+         .background(Color(UIColor.systemBlue))
+         .clipShape(RoundedRectangle(cornerRadius: 12))
+         .padding()
+
+        Text("Travel").font(.title2)
+          .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
         Spacer()
-      }
+        Spacer()
+        VStack (alignment: .leading, spacing: 20){
+          Text("Tuesday, November 1 at 12:00pm to Friday, November 11 at 11:00am" ).font(.headline)
+          
+          Text("WHAT: Fly to LAX from LGA").font(.subheadline)
+          Text("Category: Travel").font(.subheadline)
+          Text("WHERE: 1 World Way, Los Angeles, CA 90045")
+          Text("WHO: All group members")
+          Text("HOW-MUCH: $300 - $500 per round-trip ticket")
+          Text("WHY: Cheapest flight on Skyscanner with Spirit Airlines")
+          Text("[SkyScanner](https://www.skyscanner.com/)")
+          Image("skyscanner-lax-to-lga")
+            .resizable()
+            .scaledToFit()
+          Spacer()
+        } // end VStack
+        .padding(.leading, 20)
+        
+      } // end VStack
       
     } //end vstack
     
