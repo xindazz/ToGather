797b644dba23d21428c14e5e7e8187e2dc0f530a
Lisa Leung <clleung@andrew.cmu.edu>
2022-12-14 23:38:08 -0500

    styled signinview

--- ToGather/Views/SignInView.swift
+++ ToGather/Views/SignInView.swift
@@ -55,82 +55,94 @@ struct SignInView: View {
     
     var body: some View {
         VStack {
-            
-            
-            
-            
-            
-            
             VStack(alignment: .leading, spacing: 40) {
-                Text("Welcome to ToGather")
-                    .fontWeight(.heavy)
-                    .multilineTextAlignment(.center)
-                    .font(.title)
-                    .padding(EdgeInsets(top: 60, leading: 40, bottom: 0, trailing: 10))
-                
-                
-                
-                
-                Text("Let's go on a trip together")
-                    .padding(EdgeInsets(top: 40, leading: 80, bottom: 0, trailing: 10))
-                
-                
-                
+//                Text("Welcome to ToGather")
+//                    .fontWeight(.heavy)
+//                    .multilineTextAlignment(.center)
+//                    .font(.title)
+//                    .padding(EdgeInsets(top: 60, leading: 40, bottom: 0, trailing: 10))
+//
+//                Text("Let's go on a trip together")
+//                    .padding(EdgeInsets(top: 40, leading: 80, bottom: 0, trailing: 10))
+  
                 VStack {
                     Text("Email")
-                        .font(.body)
-                        .fontWeight(.bold)
+                        .font(.custom("NunitoSans-SemiBold", size: 24))
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 10))
+//                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 10))
                         .foregroundColor(.white)
 
                     
-                    TextField("123@gmail.com", text: $email)
+                    TextField(" 123@gmail.com", text: $email)
                         .textContentType(.givenName).border(Color.gray.opacity(0.5))
                         .background(Color(.white))
-                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 10))
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+
+//                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 10))
 
                     
                     Text("Password")
-                        .font(.body)
-                        .fontWeight(.bold)
+                        .font(.custom("NunitoSans-SemiBold", size: 24))
                         .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 10))
+//                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 10))
                         .foregroundColor(.white)
 
                     
-                    SecureField("Password", text: $password)
+                    SecureField(" Password", text: $password)
                         .textContentType(.givenName).border(Color.gray.opacity(0.5))
                         .background(Color(.white))
-                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 10))
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
 
-                }
+//                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 10))
+
+                } // end vstack
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+
+              Button(action: { signInUser()}) {
+                Text("Sign In")
+                  .font(.custom("NunitoSans-Bold", size: 18))
+                  .foregroundColor(.white)
+              }
+              .frame(width: 350, height:50)
+              .background(Color("primary"))
+              .clipShape(RoundedRectangle(cornerRadius: 12))
+              .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
+              .disabled(!signInProcessing && !email.isEmpty && !password.isEmpty ? false : true)
+              
+              if signInProcessing {
+                  ProgressView()
+              }
+              
+              if !signInErrorMessage.isEmpty {
+                  Text(signInErrorMessage)
+                      .foregroundColor(.red)
+              }
+              
+//                MARK: old button
+//                Button {
+//                    signInUser()
+//                } label: {
+//                    Spacer()
+//                    Text("Sign In")
+//                        .font(.headline)
+//                        .padding()
+//                        .foregroundColor(.white)
+//                    Spacer()
+//                } // end button
+//                .background(Color("buttoncolor"))                .clipShape(RoundedRectangle(cornerRadius: 12))
+//                .padding()
+//                .disabled(!signInProcessing && !email.isEmpty && !password.isEmpty ? false : true)
+//
+//                if signInProcessing {
+//                    ProgressView()
+//                }
+//
+//                if !signInErrorMessage.isEmpty {
+//                    Text(signInErrorMessage)
+//                        .foregroundColor(.red)
+//                }
+//
                 
-                Button {
-                    signInUser()
-                } label: {
-                    Spacer()
-                    Text("Sign In")
-                        .font(.headline)
-                        .padding()
-                        .foregroundColor(.white)
-                    Spacer()
-                } // end button
-                .background(Color("buttoncolor"))                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .padding()
-                .disabled(!signInProcessing && !email.isEmpty && !password.isEmpty ? false : true)
-                
-                if signInProcessing {
-                    ProgressView()
-                }
-                
-                if !signInErrorMessage.isEmpty {
-                    Text(signInErrorMessage)
-                        .foregroundColor(.red)
-                }
-                
-                
-                Divider()
                 Spacer()
             }
             .background(Color("blue"))
